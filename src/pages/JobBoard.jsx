38340300c5c0ed6872aefeffb8e9b7513a07fb8f
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-30 18:16:01 +0200

    feat: create JobBoard.jsx page to handle hero page

---
+++ src/pages/JobBoard.jsx
@@ -0,0 +1,9 @@
+import { useState, usrEffect } from "react";
+import api from "./../api/axiosInstance";
+
+const JobBoard = () => {
+    const [jobs, setJobs] = useState([]);
+
+}
+
+export default JobBoard;

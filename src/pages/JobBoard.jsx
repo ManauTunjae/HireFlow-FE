f4904254a80411543ae12d2f666b17acf843cc94
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-30 18:26:41 +0200

    feat: implement header section

--- src/pages/JobBoard.jsx
+++ src/pages/JobBoard.jsx
@@ -2,8 +2,26 @@ import { useState, usrEffect } from "react";
 import api from "./../api/axiosInstance";
 
 const JobBoard = () => {
-    const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-}
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <header className="bg-white border-b border-gray-200 py-16 text-center">
+        <div className="max-w-3xl mx-auto">
+          <h1 className="text-4xl md:text-5xl font-extrabold tracking-tight text-gray-900 mb-4">
+            Find your tech-job
+          </h1>
+          <p className="text-lg text-gray-600 max-w-xl mx-auto">
+            Explore the latest tech job opportunities and connect with top
+            companies. Your dream job is just a click away!
+          </p>
+        </div>
+      </header>
+
+      
+    </div>
+  );
+};
 
 export default JobBoard;

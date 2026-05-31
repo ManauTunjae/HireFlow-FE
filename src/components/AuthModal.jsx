ca08d54fe5763c60454f095cecb190f3d0cf3217
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-31 22:49:31 +0200

    feat: create AuthModal.jsx for login popup

---
+++ src/components/AuthModal.jsx
@@ -0,0 +1,12 @@
+import {useState, useEffect} from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const AuthModal = ({}) => {
+
+    return (
+
+    )
+}
+
+export default AuthModal;

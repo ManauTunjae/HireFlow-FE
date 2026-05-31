314b7a92cb95b67438300b3ec4186afa4c8a798c
ManauTunjae <manau.tunjae@hotmail.com>
2026-05-31 23:08:17 +0200

    feat: implement handleSubmit

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -11,7 +11,35 @@ const AuthModal = ({isOpen, OnClose}) => {
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
 
-    
+    if (!isOpen) return null;
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setLoading(true);
+        try {
+            if (isRegister) {
+                setError("Registration coming soon, try Log In instead!")
+                setLoading(false);
+                return;
+            }
+
+            const result = await login(formData.email, formData.password);
+
+            if (result.success) {
+                onclose();
+                // Just nu skickar vi dem till /dashboard, men i framtiden kan vi lägga en if-sats här:
+                // if (user.role === 'candidate') { navigate('/candidate-dashboard') }
+                navigate("/dashboard");
+            } else {
+                setError(result.message || "Invalid credential!")
+            }
+        } catch (error) {
+            setError("Something wentwrong. Please try again.")
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
 

7842a35a788c7b3689933d5059a3d4b5c48235f8
ManauTunjae <manau.tunjae@hotmail.com>
2026-06-16 10:50:02 +0200

    feat: after 7 days a job should be closing soon status

--- src/pages/JobBoard.jsx
+++ src/pages/JobBoard.jsx
@@ -46,6 +46,10 @@ const JobBoard = () => {
     if (diffDay <= 5) {
       return { text: "New", style: "bg-blue-950 text-blue-400 border border-blue-800/30 animate-pulse text-[10px] font-black uppercase px-2 py-0.5 rounded-full"}
     }
+
+    if (diffDay >= 7 && diffDay <= 14) {
+      return { text: "Closing Soon ⏳", style: "bg-amber-950 text-amber-400 border border-amber-900/30 text-[10px] font-black uppercase px-2 py-0.5 rounded-full" };
+    }
   };
 
   useEffect(() => {

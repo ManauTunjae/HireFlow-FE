a81d259d2f6a3b0c076d9ea1f39648c67cfe1556
ManauTunjae <manau.tunjae@hotmail.com>
2026-06-22 07:54:43 +0200

    feat: implement states into JobDetails.jsx

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -3,9 +3,15 @@ import { useParams, useNavigate } from "react-router-dom";
 import api from "../api/axiosInstance";
 
 const JobDetails = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [job, setJob] = useState(null);
+    const [Loading, setLoading] = useState(true);
 
+    
     return (
-
+        <>
+        </>
     )
 }
 export default JobDetails;

7ea7ea7255b0ee9611ca5cfbbc963b607cde0421
ManauTunjae <manau.tunjae@hotmail.com>
2026-06-22 08:00:39 +0200

    feat: implement useEffect to fetching a job data

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -3,15 +3,24 @@ import { useParams, useNavigate } from "react-router-dom";
 import api from "../api/axiosInstance";
 
 const JobDetails = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [job, setJob] = useState(null);
-    const [Loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [job, setJob] = useState(null);
+  const [Loading, setLoading] = useState(true);
 
-    
-    return (
-        <>
-        </>
-    )
-}
+  useEffect(() => {
+    const fetchJobDetails = async () => {
+      try {
+        const response = await api.get(`api/jobs/${id}`);
+        setJob(response.data);
+      } catch (error) {
+        console.error("Could not fetching job details:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobDetails();
+  }, [id]);
+  return <></>;
+};
 export default JobDetails;
